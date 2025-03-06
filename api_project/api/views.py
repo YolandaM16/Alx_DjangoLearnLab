deea3f06fb0e568f20ae82cf262389db97900235
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-06 21:06:41 +0200

    modified new files

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, ListModelMixin
-from .models import Book
 from .serializers import BookSerializer
+from .models import Book
 
-# Create your views here
-class BookList(generics.ListAPIView):
+# Create your views here.
+# class BookList(generics.ListAPIView):
+#     queryset = Book.objects.all()
+    
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer

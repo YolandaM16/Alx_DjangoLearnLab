e214068f308eed8c1ac06b7cede30324cc8e1d82
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-18 21:30:03 +0200

    new file

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Book
+
+class BookAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.book_data = {'title': 'HTGAWM', 'author': 'Viola Davis', 'isbn': 20241117}
+        self.book = Book.objects.create(**self.book_data)
+
+    def test_create_book(self):
+        response = self.client.post('/api/books/', self.book_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["title"], "HTGAWM")
+
+    def test_get_books(self):
+        response = self.client.get('/api/books/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreaterEqual(len(response.data), 1)
+
+    def test_update_book(self):
+        updated_data = {"title": "PYTHON", "author": "Viola Davis", "isbn": "20241117"}
+        response = self.client.put(f'/api/books/{self.book.id}/', updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], "PYTHON")
+
+    def test_delete_book(self):
+        response = self.client.delete(f'/api/books/{self.book.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Book.objects.filter(id=self.book.id).exists())
+

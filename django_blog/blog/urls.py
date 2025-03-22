e59a81aa73d8707199c5c470ee526a62cf1e1144
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-22 18:13:16 +0200

    modified urls file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
         path("", PostListView.as_view(), name="post-list"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post_update"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 ]

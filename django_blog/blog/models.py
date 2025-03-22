a2e47b65cab1ac35a703c4ec8710bf9d31a79dfc
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-22 20:46:38 +0200

    modified models file

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,8 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    ordering = ['name']
+
+def __str__(self):
+    return self.name
+
+def get_absolute_url(self):
+    return reverse('blog:tag_posts', args=[self.slug])
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+
+class Meta:
+    ordering = ['-publish']
+    indexes = [
+    models.Index(fields=['-publish']),
+]
+
+def __str__(self):
+    return self.title
+
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()

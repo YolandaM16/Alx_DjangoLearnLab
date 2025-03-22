a98778fb69c8d19ac17684135121c03b956911ed
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-22 19:33:05 +0200

    added Comment class

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -22,3 +22,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.title}'
+    
+    class Meta:
+        ordering = ['created_date']
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
     path('accounts/login/', LoginView.as_view(template_name='blog/login.html'), name="login"),
     path('accounts/logout/', LogoutView.as_view(template_name='blog/logout.html'), name="logout"),
     path('accounts/profile/', Profile, name="profile"),
-    path('home/', home, name="home"),
     path('posts/', PostListView.as_view(), name="post_list"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
     path('post/new/', PostCreateView.as_view(), name='post_create'),

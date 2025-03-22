e884e07e8fce8073023267b60b560f3b13767a40
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-22 20:30:11 +0200

    forms file updated

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 from django import forms
+from django import Comment
 
 #class CustomUserCreationForm(UserCreationForm):
 #   email = forms.EmailField(required=True)
@@ -22,3 +23,9 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["bio", "profile_pic"]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']

f6d3f6dfe27cd896caf3cb7a3e80ca5ff2000c04
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-30 22:52:32 +0200

    modified views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from rest_framework import filters
+from .serializers import PostSerializer, CommentSerializer
 
 # Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing posts.
+    Supports CRUD operations with permissions and search filtering.
+    """
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content'] 
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user) 
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing comments.
+    Supports CRUD operations with permissions.
+    """
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)  

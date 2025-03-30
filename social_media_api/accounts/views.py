d0aaa59d61c84ac3c586edb947fbdaccbe21c20a
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-30 22:20:24 +0200

    modified views

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from .models import CustomUser
 from rest_framework import generics
-from rest_framework import APIView
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
